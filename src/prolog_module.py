569d4443dac46fdfcbd9e5236f46140f729cf620
Alberto Olivares Alarcos <aolivares@iri.upc.edu>
2023-07-11 16:34:39 +0200

    axon working with specificity 1 and the plan neems

--- src/prolog_module.py
+++ src/prolog_module.py
@@ -16,11 +16,14 @@ What is defined in it?
 """
 
 owl_uri_to_label_dict = {"http://www.iri.upc.edu/groups/perception/OCRA/ont/ocra.owl#": "ocra", \
+                         "http://www.iri.upc.edu/groups/perception/OCRA/ont/ocra_common.owl#": "ocra_common", \
                          "http://www.iri.upc.edu/groups/perception/OCRA/ont/ocra_filling_a_tray.owl#": "ocra_filling_a_tray", \
+                         "http://www.iri.upc.edu/groups/perception/OCRA/maps/piling_cloth.owl#": "map_piling_cloth", \
                          "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#": "dul", \
                          "http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf"
 }
 
 app_ontology_label = "ocra_filling_a_tray"
+semantic_map_namespace_cloth = "map_piling_cloth"
 
 domain_ontology_label = "ocra"
