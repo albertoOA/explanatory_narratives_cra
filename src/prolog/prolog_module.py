5e8d76bcd171b37d15f11a8a1b4bd10d8a8b584a
Alberto Olivares Alarcos <aolivares@iri.upc.edu>
2023-09-13 19:30:48 +0200

    added some URIs to dict

--- src/prolog/prolog_module.py
+++ src/prolog/prolog_module.py
@@ -63,8 +63,10 @@ owl_uri_to_label_dict = {"http://www.iri.upc.edu/groups/perception/OCRA/ont/ocra
                          "http://www.iri.upc.edu/groups/perception/OCRA/ont/ocra_filling_a_tray.owl#": "ocra_filling_a_tray", \
                          "http://www.iri.upc.edu/groups/perception/OCRA/maps/piling_cloth.owl#": "map_piling_cloth", \
                          "http://www.iri.upc.edu/groups/perception/OCRA/maps/bringing_object.owl#": "map_bringing_object", \
+                         "http://www.ease-crc.org/ont/SOMA.owl#": "soma", \
                          "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#": "dul", \
                          "http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf", \
+                         "http://www.w3.org/2000/01/rdf-schema#": "rdfs", \
                          "http://www.w3.org/2002/07/owl#": "owl"
 }
 
