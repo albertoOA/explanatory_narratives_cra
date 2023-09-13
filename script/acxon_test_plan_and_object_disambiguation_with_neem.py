624ce1ae7adcc06be504ff033f387d7dc636ce99
Alberto Olivares Alarcos <aolivares@iri.upc.edu>
2023-09-13 19:40:22 +0200

    minor change

--- script/acxon_test_plan_and_object_disambiguation_with_neem.py
+++ script/acxon_test_plan_and_object_disambiguation_with_neem.py
@@ -133,8 +133,10 @@ if __name__ == '__main__':
       combined_c_narrative_mod = combined_c_narrative_mod.replace(pair_of_plans_to_c_narrate_name[1], "Plan_B")
       combined_c_narrative_mod = combined_c_narrative_mod.replace("has worse quality value", "has a lower value")
       combined_c_narrative_mod = combined_c_narrative_mod.replace("has better quality value", "has a larger value")
+      combined_c_narrative_mod = combined_c_narrative_mod.replace("has equivalent quality value than", "has the same value as")
       combined_c_narrative_mod = combined_c_narrative_mod.replace("has role", "is classified as")
       combined_c_narrative_mod = combined_c_narrative_mod.replace("is role of", "classifies")
+      combined_c_narrative_mod = combined_c_narrative_mod.replace("defines task", "includes task")
 
       ## print("\nC-Narrative modified")
       ## print(combined_c_narrative_mod)
